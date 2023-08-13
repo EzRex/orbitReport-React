f6b3df586cb78af5ce0593682bf68cbf38739821
Ezekielle Rex Duller <ezekielle.duller@gmail.com>
2023-08-13 15:07:36 -0400

    Finished assignment, didnt do styling

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,13 +3,29 @@ const Table = ({ sat }) => {
       <table>
        <thead>
         <tr>
-          <th>Header TBD</th>
+          <th>Name</th>
+          <th>Type of Sattelite</th>
+          <th>Launch Date</th>
+          <th>Status</th>
         </tr>
         </thead>
         <tbody>
-        <tr>
-          <td>Row Data TBD</td>
-        </tr>
+         {sat.map((data,id) => {
+            console.log(sat)
+            //console.log(data)
+            console.log(data)
+            //console.log(id)
+            return(
+              <tr key={id}>
+                <td>{data.name}</td>
+                <td>{data.type}</td>
+                <td>{data.launchDate}</td>
+                <td>{data.operational ? "Active" : "Inactive"}</td>
+              </tr>
+
+            )
+          })
+        }
         </tbody>
       </table>
   );
