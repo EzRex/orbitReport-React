eab791e7993c2cbee6eb3a1abd78aa08526db66d
Ezekielle Rex Duller <ezekielle.duller@gmail.com>
2023-08-13 12:08:59 -0400

    Initial work on assignment

---
+++ src/components/Banner.js
@@ -0,0 +1,11 @@
+
+export default function Banner() {
+    return(
+        <div>
+            <header>Orbit Report</header
+            ><p>
+                Click on the buttons to see the sattelites in that orbit type
+            </p>
+        </div>
+    );
+}
